93a407eda458cedb671dc7ce1a167e22999420d9
Wubai Zhou <zhouwubai@gmail.com>
2014-03-02 17:54:41 -0500

    push nlp

---
+++ src/main/java/fiu/kdrg/bcin/citysafety/preprocess/Constants.java
@@ -0,0 +1,12 @@
+package fiu.kdrg.bcin.citysafety.preprocess;
+
+public class Constants {
+
+	public static String LOCATION_ENTITY = "LOCATION";
+	public static String DATE_ENTITY = "DATE";
+	public static String DURATION_ENTITY = "DURATION";
+	public static String TIME_ENTITY = "TIME";
+	
+	
+	
+}
---
+++ src/main/java/fiu/kdrg/bcin/citysafety/preprocess/ParagraphPreprocessor.java
@@ -0,0 +1,71 @@
+package fiu.kdrg.bcin.citysafety.preprocess;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+public class ParagraphPreprocessor {
+
+	private static StanfordCoreNLP pipeline;
+	
+	static{
+		Properties props = new Properties();
+		props.put("annotators", "tokenize, ssplit, pos, lemma, ner");
+		props.put("ner.model.3class", "");
+		props.put("ner.model.MISCclass", "");
+		pipeline = new StanfordCoreNLP(props);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	
+	
+	public String removeEntity(String paragraph, Set<String> entityFilter){
+		
+		Annotation document = new Annotation(paragraph);
+		pipeline.annotate(document);
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+		StringBuilder sb = new StringBuilder();		
+		
+		for(CoreMap sentence : sentences){
+			String type;
+			String sentenceText = sentence.toString();
+			
+			for(CoreLabel token: sentence.get(TokensAnnotation.class)){
+				type = token.get(NamedEntityTagAnnotation.class);
+				//remove this token from original text
+				if(entityFilter.contains(type)){
+					sentenceText = sentenceText.replace(token.get(TextAnnotation.class) + " ", "");
+				}
+			}
+			
+			sb.append(sentenceText);
+			sb.append("\t\n");
+		}
+		
+		return sb.toString();
+	}
+	
+	
+	public String removeStopWords(String paragraph){
+		
+		return "";
+	}
+	
+	
+}
